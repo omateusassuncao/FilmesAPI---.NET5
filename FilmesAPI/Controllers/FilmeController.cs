fc4674e5ba56e92f550c8000c711ec574da80b29
Mateus Vieira da Assunção <mateusvassuncao@gmail.com>
2022-09-11 22:22:43 -0300

    Add project files.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+    }
+}

3d77d712abf21424c7e4aeec70205cc3f8cc5cfc
Mateus Vieira da Assunção <mateusvassuncao@gmail.com>
2022-09-11 23:23:38 -0300

    Utilizando mais propriedades do annotation

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -6,10 +6,13 @@ namespace FilmesAPI.Models
     {
         [Required(ErrorMessage = "O campo título é obrigatório")]
         public string Titulo { get; set; }
-        [Required(ErrorMessage = "O campo diretor é obrigatório")]
+
+        [Required(ErrorMessage = "O campo diretor é obrigatório", AllowEmptyStrings=true)]
         public string Diretor { get; set; }
+
         [StringLength(30, ErrorMessage ="Gênero não deve ultrapassar 30 caracteres")]
         public string Genero { get; set; }
+
         [Range(1, 600, ErrorMessage = "Duração deve ser entre 1 e 600 minutos")]
         public int Duracao { get; set; }
     }
